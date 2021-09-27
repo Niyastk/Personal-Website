d017faf24d61fe02b970f0d52560823ebbf8c196
Niyastk <niyastk9562@gmail.com>
2021-09-27 23:02:18 +0530

    updated jquery

--- form-validation.js
+++ form-validation.js
@@ -1,29 +1,102 @@
 $(function () {
-  $('#submit-form').validate({
-    rules: {
-      name: {
-        required: true,
-        minlength: 5
-      },
-      email: {
-        required: true,
-        email: true
-      },
-      subject: {
-        required: true,
-        minlength: 10
-      }
-    },
-    messages: {
-      name: {
-        minlength: 'Enter Minimum of 5 characters'
-      },
-      email: {
-        email: 'Enter a Valid Email'
-      },
-      subject: {
-        minlength: 'Enter Minimum of 10 characters '
-      }
+  $('#name-error-message').hide()
+  $('#email-error-message').hide()
+  $('#subject-error-message').hide()
+  $('#message-error-message').hide()
+
+  var error_name = false
+  var error_email = false
+  var error_subject = false
+  var error_message = false
+
+  $('#name').focusout(function () {
+    check_name()
+  })
+
+  $('#email').focusout(function () {
+    check_email()
+  })
+
+  $('#subject').focusout(function () {
+    check_subject()
+  })
+
+  $('#message').focusout(function () {
+    check_message()
+  })
+
+  function check_name () {
+    var content = /^[a-zA-Z]*$/
+    var name = $('#name').val()
+
+    if (content.test(name) && name !== '') {
+      $('#fname_error_message').hide()
+    } else {
+      $('#name-error-message').html('Only Characters are allowed')
+      $('#name-error-message').show()
+      $('#name').css('border-bottom', '2px solid #F90A0A')
+      error_name = true
+    }
+  }
+
+  function check_email () {
+    var content = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/
+    var email = $('#email').val()
+    if (content.test(email) && email !== '') {
+      $('#email-error-message').hide()
+    } else {
+      $('#email-error-message').html('Enter a Valid Email')
+      $('#email-error-message').show()
+      $('#email').css('border-bottom', '2px solid #F90A0A')
+      error_email = true
+    }
+  }
+
+  function check_subject () {
+    var subject = $('#subject').val()
+    if (subject !== '') {
+      $('#subject-error-message').hide()
+    } else {
+      $('#subject-error-message').html('Type a Subject')
+      $('#subject-error-message').show()
+      $('#subject').css('border-bottom', '2px solid #F90A0A')
+      error_subject = true
+    }
+  }
+  function check_message () {
+    var message = $('#subject').val()
+    if (message !== '') {
+      $('#message-error-message').hide()
+    } else {
+      $('#message-error-message').html('Type a Message')
+      $('#message-error-message').show()
+      $('#message').css('border-bottom', '2px solid #F90A0A')
+      error_subject = true
+    }
+  }
+
+  $('submit-form').submit(function () {
+    error_name = false
+    error_email = false
+    error_subject = false
+    error_message = false
+
+    check_name()
+    check_email()
+    check_subject()
+    check_message()
+
+    if (
+      error_name === false &&
+      error_email === false &&
+      error_subject === false &&
+      error_message === false
+    ) {
+      alert('Registration Successful')
+      return true
+    } else {
+      alert('Please Check and Fill the Form')
+      return false
     }
   })
 })
